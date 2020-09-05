360bd6c866c506e6156e2bd6ed39dbff9b9171bb
izik4004 <49079739+izik4004@users.noreply.github.com>
2020-09-05 14:36:24 +0100

    my php project

---
+++ create.php
@@ -0,0 +1,29 @@
+<?php
+include ('template/header.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<form>
+  <div class="form-row">
+      <h3> Add New Blog Content </h3></br>
+    <div class="form-group col-md-10"></br>
+      <input type="text" class="form-control" id="inputEmail4" placeholder="Blog Title">
+    </div>
+    <div class="form-group col-md-6">
+      <label for="inputPassword4">Description</label>
+      <textarea id="inputtext"  rows="10" cols="80"></textarea>
+      
+    </div>
+  </div>
+  
+    
+  </div>
+  </div>
+  <button type="submit" class="btn btn-outline-primary">Save</button>
+  <button type="submit" class="btn btn-outline-success">Publish</button>
+</form>
+
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+include ('template/header.php');
+
+
+?>
+
+<!DOCTYPE html>
+<html >
+    
+<body class="text-center">
+
+    
+      <main role="main" class="inner cover">
+        <h1 class="cover-heading">Cover your page.</h1>
+        <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+        <p class="lead">
+          <a href="create.php" class="btn btn-lg btn-outline-primary">Add Post</a>
+          <a href="#" class="btn btn-lg btn-outline-primary">View Existing</a>
+          
+        </p>
+      </main>
+
+
+</body>
+</html>
